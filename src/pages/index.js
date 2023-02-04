cfac88080c5a0810eea422a485e1d41133ead508
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-03 22:00:41 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const Index = () => {
 
   return (
     <div className="flex flex-col items-center h-screen bg-white">
-      <h1>YT-Recap</h1>
+      <h1 className="text-black text-3xl font-bold p-8">YT-Recap</h1>
       <form onSubmit={handleSubmit} className="m-auto">
         <input
           className="bg-gray-200 p-2 rounded-lg w-64 mt-5"
@@ -20,7 +20,7 @@ const Index = () => {
           onChange={(e) => setInput(e.target.value)}
         />
         <Link href="/summary">
-          <button className="bg-sky-200 text-white p-2 rounded-lg ml-2 hover:bg-sky-400 mt-5">
+          <button className="bg-rose-500 text-white p-2 rounded-lg ml-2 hover:bg-rose-700 mt-5">
             Go!
           </button>
         </Link>
