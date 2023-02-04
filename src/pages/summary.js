c73d1b27c63269e19aa93b56c97a1c5554d2bdb5
Nicholas Tillmann <ntillmann1439@gmail.com>
2023-02-03 21:57:40 -0500

    adding dependencies

---
+++ src/pages/summary.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Summary = () => {
+  return (
+    <div className="flex items-center h-screen bg-white">
+      <div className="m-auto p-10 text-center">
+        <h1 className="text-xl text-sky-200">Summary Title</h1>
+        <p className="mt-5 text-sky-400">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
+          velit vel velit tempor, eget luctus diam commodo. Nam ut sollicitudin
+          mi. Sed non elit euismod, faucibus lacus et, vestibulum lorem. Aliquam
+          ullamcorper quis neque in egestas.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
